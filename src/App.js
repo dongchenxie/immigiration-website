c9bd2a30078538cec8de6885013f461484b79b69
xdc811 <37362503+xdc811@users.noreply.github.com>
2020-10-12 18:10:48 +0800

    update home

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: Dongchen Xie
+ * @Date: 2020-10-12 08:21:44
+ * @LastEditors: Dongchen Xie
+ * @LastEditTime: 2020-10-12 11:42:13
+ * @Description: file content
+ */
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -8,14 +15,24 @@ import {
   Link,
   useParams
 } from "react-router-dom";
+import {
+  createMuiTheme,
+  makeStyles,
+  ThemeProvider
+} from "@material-ui/core/styles";
 import Navbar from "./widget/Navbar.js"
 import LanguageRoute from "./routes/languageRoute.js"
+import style from './style/GlobleStyle'
+import Footer from "./widget/Footer"
 function App() {
   return (
+    <ThemeProvider >
     <Router>
      <Navbar/>
      <LanguageRoute/>
+     <Footer/>
   </Router>
+  </ThemeProvider>
   );
 }
 
--- src/routes/languageRoute.js
+++ src/routes/languageRoute.js
@@ -1,12 +1,26 @@
+/*
+ * @Author: Dongchen Xie
+ * @Date: 2020-10-12 08:21:44
+ * @LastEditors: Dongchen Xie
+ * @LastEditTime: 2020-10-12 14:42:33
+ * @Description: file content
+ */
 import React from "react";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link,
-  useParams
+  Redirect,
+  useParams,
+  useRouteMatch
 } from "react-router-dom";
 import useStore from "../zustand/zustand"
+import Home from "../pages/Home"
+import About from "../pages/About"
+import Service from "../pages/Service"
+import Blog from "../pages/Blog"
+import Contact from "../pages/ContactUs"
 // Params are placeholders in the URL that begin
 // with a colon, like the `:id` param defined in
 // the route in this example. A similar convention
@@ -17,6 +31,7 @@ export default function ParamsExample() {
   return (
         <Switch>
           <Route path="/:lang" children={<Child />} />
+          <Route path="/" children={<Redirect to={"/en"}/>}/>
         </Switch>
   );
 }
@@ -25,13 +40,19 @@ function Child() {
   // We can use the `useParams` hook here to access
   // the dynamic pieces of the URL.
   let {lang} = useParams();
+  let { path, url } = useRouteMatch();
   const setLanguage = useStore(s=>s.setLanguage)
   React.useEffect(()=>{
     setLanguage(lang)
   },[lang])
   return (
-    <div>
-      <h3>ID: {lang}</h3>
-    </div>
+    <Switch>
+       <Route path={`${url}/contact`} children={<Contact/>} />
+       <Route path={`${url}/blog`} children={<Blog/>} />
+   
+      <Route path={`${url}/service`} children={<Service/>} />
+      <Route path={`${url}/about`} children={<About/>} />
+          <Route path={`${url}/`} children={<Home/>} />
+        </Switch>
   );
 }
